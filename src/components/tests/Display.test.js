e55c9117d52180f97fcc5f79f5ba4d51cdff3ca4
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2022-01-25 17:52:18 -0500

    finished

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -5,11 +5,50 @@ import userEvent from '@testing-library/user-event';
 
 import Display from './../Display';
 
+import mockFetchShow from './../../api/fetchShow';
+jest.mock('./../../api/fetchShow');
+
+const testShow = {
+    name: 'test show',
+    summary: '80s kids',
+    seasons: [
+        {
+            id: 0,
+            name: 'season 1',
+            episodes: []
+        },
+        {
+            id: 1,
+            name: 'season 2',
+            episodes: []
+        },
+    ]
+};
 
 test('renders without errors with no props', ()=>{
-
+    render(<Display />);
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test('renders Show component when the button is clicked ', async ()=>{
+    mockFetchShow.mockResolvedValueOnce(testShow);
+    render(<Display/>);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+    const show = await screen.findByTestId('show-container');
+    expect(show).toBeInTheDocument();
+});
+
+test('renders show season options matching your data when the button is clicked', async ()=>{
+    mockFetchShow.mockResolvedValueOnce(testShow);
+    render(<Display/>);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    await waitFor(()=> {
+        const seasonOptions = screen.queryAllByTestId('season-option');
+        expect(seasonOptions).toHaveLength(2);
+
+    })
+
+});
