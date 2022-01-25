75444a59760fcc5f3f9302db77d8d0bbe7001932
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2022-01-25 11:20:51 -0500

    stuck

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,9 +3,41 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
+const testEpisode = {
+    id: 15,
+    image: 'string',
+    name: 'Coco',
+    season: 4,
+    number: 7,
+    summary: 'A good episode',
+    runtime: 58,
+  }
 
-test("renders without error", () => {});
+  const testEpisodeWithoutImg = {
+    id: 15,
+    image: null,
+    name: 'Coco',
+    season: 4,
+    number: 7,
+    summary: 'A good episode',
+    runtime: 58,
+  }
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders without error", () => {
+    render(<Episode episode={testEpisode}/>)
+});
 
-test("renders default image when image is not defined", ()=>{});
+test("renders the summary test passed as prop", ()=>{
+    render(<Episode episode={testEpisode}/>);
+    const summary = screen.queryByText(/a good episode/i);
+    expect(summary).toBeInTheDocument();
+    expect(summary).toBeTruthy();
+    expect(summary).toHaveTextContent(/a good episode/i);
+});
+
+test("renders default image when image is not defined", ()=>{
+    render(<Episode episode={testEpisodeWithoutImg}/>);
+    const image = screen.queryByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png');
+    console.log(image)
+    expect(image).toBeInTheDocument();
+});
